43e20245df7340cc5a191982222a4c97357904a0
tessamorris <tessam@uci.edu>
2018-08-28 14:08:05 -0700

    Replace YBSimpleSeg with segment image This method will calculate the "background and subtract it from the original image`

--- functions/YBSimpleSeg.m
+++ functions/YBSimpleSeg.m
@@ -1,21 +1,8 @@
 % Copyright (C) 2016 Nils Persson 
+% Modified by Tessa Morris 
 
-function ims = YBSimpleSeg(ims)
+function ims = YBSimpleSeg( gray_im )
 
-%YBSeg Yanowitz-Bruckstein image segmentation with fiber unentanglement
-%   SP is the structure path
-%   File is the file path from current active dir
-%   Dim is the image dimension in nm
 
-ims.edge = edge(ims.gray,'canny');         % Apply a Canny edge finder - 1's at the edges, 0's elsewhere
-ims.grayDouble = double(ims.gray);                     % Turn the grey image into double prec.
-ims.edgeDouble = double(ims.edge);                     % Turn the edge image into double prec.
-ims.initThresh = ims.grayDouble.*ims.edgeDouble;                        % Fill in the grey values of the edge pixels in a new image file                      
-
-ims.threshSurf = YBiter(ims.initThresh);                    % Perform Yanowitz-Bruckstein surface interpolation to create threshold surface from edge gray values
-
-ims.yb_bw = ims.gray>ims.threshSurf;                          % Segment the image; pixels above threshold surface are white, if not, black
-ims.std_bw = im2bw(ims.img);
-ims.yb_gray = mat2gray(ims.grayDouble-ims.threshSurf);
 
 end
--- functions/segmentImage.m
+++ functions/segmentImage.m
@@ -1,7 +1,36 @@
-function [ output_args ] = segmentImage( input_args )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function [  ] = segmentImage( im )
+%This function will segment the image using the Yanowitz-Bruckstein 
+%image segmentation with fiber unentanglement
 
+% Convert the image to be grayscale. 
+[ gray_im ] = makeGray( im );
+
+% Convert to double precision 
+grayDouble = double(ims.gray);
+
+% Apply a Canny edge finder - 1's at the edges, 0's elsewhere
+edges = edge( gray_im,'canny' );
+
+% Convert to double precision 
+edgesDouble = double(edges);
+
+% Fill in the grey values of the edge pixels in a new image file                      
+initThresh = ims.grayDouble.*ims.edgeDouble;
+
+% Perform Yanowitz-Bruckstein surface interpolation to create threshold
+% surface from edge gray values
+threshSurf = YBiter(ims.initThresh);
+
+% Segment the image. Pixels above threshold surface are white, black
+% otherwise
+yb_bw = gray_im > threshSurf;
+
+% Convert image to binary image by thresholding
+std_bw = im2bw( im );
+
+% Subtract the surface from the gray image and convert the matrix to an
+% intensity image 
+yb_gray = mat2gray( grayDouble - threshSurf );
 
 end
 
