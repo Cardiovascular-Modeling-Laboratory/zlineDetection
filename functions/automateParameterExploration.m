f1f19dbd7aba918a0730d968d22f515204a9abeb
tessamorris <tessam@uci.edu>
2018-11-06 08:24:01 -0800

    Add last changes before modifying

--- functions/automateParameterExploration.m
+++ functions/automateParameterExploration.m
@@ -175,7 +175,8 @@ for s = 1:sn
 
             %Append the summary file 
             save(fullfile(image_path{1}, summary_file_name), ... 
-                'all_lengths', 'all_medians', '-append');
+                'all_lengths', 'all_medians', 'all_diffusiontimes',...
+                'all_sigma', 'all_rho', '-append');
 
             %Close all of the images 
             close all; 
@@ -183,86 +184,4 @@ for s = 1:sn
         end 
     end 
 end 
-
-
-
-% Loop through all of the image files 
-for k = 1:n 
-    % Store the current filename 
-    filename = strcat(image_path{1}, image_files{1,k});
-    
-    % Perform the analysis including saving the image 
-    im_struct = analyzeImage( filename, settings ); 
-    
-    % If the user wants to calculate continuous z-line length 
-    if settings.tf_CZL 
-
-        if k == 1
-            %Create a cell to store all distances 
-            all_lengths = cell(1,n); 
-            all_medians = cell(1,n); 
-            
-            %If there is more than one image being analyzed, create a summary
-            %file 
-            if n > 1
-                %Get today's date in string form.
-                date_format = 'yyyy_mm_dd';
-                today_date = datestr(now,date_format);
-
-                %Create a summary file name 
-                summary_file_name = strcat(today_date, ...
-                    '_zline_summary.mat');
-
-                %Save and create a summary file
-                save(fullfile(image_path{1}, summary_file_name), ...
-                    'all_lengths', 'all_medians', 'image_files');
-            end 
-        end 
-        
-        %Calculate the continuous z-line length 
-        all_lengths{1,k} = continuous_zline_detection(im_struct, settings); 
-        
-        %Compute the median
-        all_medians{1,k} = median( all_lengths{1,k} ); 
-        
-        %Create a histogram of the distances
-        figure; histogram(all_lengths{1,k});
-        set(gca,'fontsize',16)
-        hist_name = strcat('Median: ', num2str(all_medians{1,k}),' \mu m');
-        title(hist_name,'FontSize',18,'FontWeight','bold');
-        xlabel('Continuous Z-line Lengths (\mu m)','FontSize',18,...
-            'FontWeight','bold');
-        ylabel('Frequency','FontSize',18,'FontWeight','bold');
-        
-        %Save histogram as a tiff 
-        fig_name = strcat( im_struct.im_name, '_CZLhistogram');
-        saveas(gcf, fullfile(im_struct.save_path, fig_name), 'tiffn');
-        
-        %If there is more than one FOV, save a summary file
-        if n > 1 
-            %Append the summary file 
-            save(fullfile(image_path{1}, summary_file_name), ... 
-                'all_lengths','all_medians','all_diffusiontimes',... 
-                'all_sigma', 'all_rho''-append');
-        end 
-        
-        %Close all of the images 
-        close all; 
-        
-    end 
-
-    % If the user wants to calculate OOP
-    if settings.tf_OOP && k == 1
-        disp('NOT YET IMPLEMENTED: OOP'); 
-        %settings.cardio_type
-    end 
-    
-    % Close all figures
-    close all; 
-    
-    % Clear the file name 
-    clear filename
-   
-end 
-
 
