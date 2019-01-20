faa65984c6876f0183a53a5bed35a053e740be33
tessamorris <tessam@uci.edu>
2019-01-20 14:38:23 -0800

    Create function to compute orientation information for an image This should encapsulate the similarities between actinDetect and sarcDetect

--- functions/actinDetection.m
+++ functions/actinDetection.m
@@ -33,10 +33,104 @@
 % Irvine, CA  92697-2700
 
 
-function [ output_args ] = actinDetection( input_args )
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+function [ output_args ] = actinDetection( filenames, settings )
 
+% Save the Options struct from settings 
+Options = settings.Options;
+
+% Store the image information
+[ im_struct ] = storeImageInfo( filenames.actin );
+
+%Create a grayscale version of the image (if it was not already in
+%grayscale) 
+[ im_struct.gray ] = makeGray( im_struct.img ); 
+
+% Create a new folder in the image directory with the same name as the 
+% image file if it does not exist. If it does exist, add numbers until it
+% no longer exists and then create it 
+create = true; 
+new_subfolder = ...
+    addDirectory( im_struct.im_path, im_struct.im_name, create ); 
+
+% Save the name of the new path 
+im_struct.save_path = fullfile(im_struct.im_path, new_subfolder); 
+
+%%%%%%%%%%%%%%%%%%%%%%%% Run Coherence Filter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Coherence-Enhancing Anisotropic Diffusion Filtering, which enhances
+% contrast and calculates the orientation vectors for later usage. 
+% The parameters (supplied by the GUI) are (1) Orientation Smoothing and
+% (2) Diffusion Time 
+
+% Start a wait bar 
+disp('Diffusion Filter...');
+
+% Inputs are the grayscale image and the Options struct from settings. 
+% The output is the diffusion filtered image and eigenvectors - Not sure
+% why this is important, but... 
+[ im_struct.CEDgray, im_struct.v1x, im_struct.v1y ] = ...
+    CoherenceFilter( im_struct.gray, settings.Options );
+
+% Clear the command line 
+clc; 
+
+% Convert the matrix to be an intensity image 
+im_struct.CEDgray = mat2gray( im_struct.CEDgray );
+
+% If the user would like to display the filtered image, display it
+if settings.disp_df
+    % Open a figure and display the image
+    figure; imshow( im_struct.CEDgray );
+    
+    % Save the figure. 
+    imwrite( im_struct.CEDgray, fullfile(save_path, ...
+        strcat( im_struct.im_name, '_DiffusionFiltered.tif' ) ),...
+        'Compression','none');
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%% Run Top Hat Filter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Update waitbar 
+disp('Top Hat Filter...');
+
+%Compute the top hat filter using the disk structuring element with the
+%threshold defined by the user input tophat filter. It then adjusts the
+%image so that 1% of data is saturated at low and high intensities of the
+%image 
+im_struct.CEDtophat = ...
+    imadjust( imtophat( im_struct.CEDgray, ...
+    strel( 'disk', settings.tophat_size ) ) );
+
+% If the user would like to display the filtered image, display it
+if settings.disp_tophat
+    % Open a figure and display the image
+    figure; imshow( im_struct.CEDtophat ); 
+    
+    % Save the figure. 
+    imwrite( im_struct.CEDtophat, fullfile(save_path, ...
+        strcat( im_struct.im_name, '_TopHatFiltered.tif' ) ),...
+        'Compression','none');
+    
+end
+
+%%%%%%%%%%%%%%%%%% Calculate Orientation Vectors %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Calculate orientation vectors
+[im_struct.orientim, im_struct.reliability] = ...
+    ridgeorient(im_struct.CEDtophat, ...
+    Options.sigma, Options.rho, Options.rho);
+
+%%%%%%%%%%%%%%%%%%%%%%%%% Threshold and Clean %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
+% Update waitbar 
+disp('Threshold and Clean...');
+
+% Use adaptive thresholding to convert to black and white.  
+[ im_struct.CEDbw, im_struct.surface_thresh ] = ...
+    segmentImage( im_struct.CEDtophat ); 
+
+% Remove regions that are not reliable (less than 0.5)
+im_struct.CEDbw( im_struct.reliability < settings.reliability_thresh) = 0; 
 
 end
 
---
+++ functions/orientInfo.m
@@ -0,0 +1,12 @@
+% ORIENT ANALYSIS - this function will report the orientation information
+% of an image 
+
+function [ output_args ] = orientInfo( im )
+
+%Create a grayscale version of the image (if it was not already in
+%grayscale) 
+[ grayIM ] = makeGray( im ); 
+
+
+end
+
