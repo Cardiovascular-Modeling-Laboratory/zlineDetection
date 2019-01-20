35e4766c72938a8852bd3e6ac2f99b81c11394b3
tessamorris <tessam@uci.edu>
2019-01-20 12:32:53 -0800

    Add selecting zline and actin images Need t update settings to pull all of the info from the gui

--- functions/runDirectory.m
+++ functions/runDirectory.m
@@ -18,17 +18,53 @@ function [  ] = runDirectory( settings )
 %This function will take file names as an input and then loop through them,
 %calling the analyze function
 
+    
 % Prompt the user to select the images they would like to analyze. 
-[ image_files, image_path, n ] = ...
-    load_files( {'*.TIF';'*.tif';'*.*'} ); 
+[ zline_images, zline_path, zn ] = ...
+    load_files( {'*w1mCherry*.TIF';'*w1mCherry*.tif';'*.*'}, ...
+    'Select images stained for z-lines...'); 
 
 % Stop if the user hit the cancel button
-if isequal(image_path, 0); return; end
+if isequal(zline_path, 0); 
+    disp('No z-line files selected. Press "Run Folder" to try again.'); 
+    return; 
+end
+
+
+% If the user would like to filter with actin, have them select the actin
+% images 
+if settings.actin_filt == true
+    [ actin_images, actin_path, an ] = ...
+        load_files( {'*GFP*.TIF';'*GFP*.tif';'*.*'}, ...
+        'Select images stained for actin...');
+    
+    % Make sure that the actin files were selected.
+    if isequal(actin_path, 0); 
+        disp('No actin files selected. Press "Run Folder" to try again.'); 
+        return; 
+    end
+    
+    % If the number of actin and z-line files are not equal, have the user
+    % try again. 
+    if an ~= zn
+        disp('The number of z-line files does not equal the number of actin files.'); 
+        disp(strcat('Actin Images: ', num2str(an), ...
+            'Z-line Images: ', num2str(zn))); 
+        disp('Press "Run Folder" to try again.'); 
+        return; 
+    end
+    
+else
+    actin_images = NaN; 
+    actin_path = NaN; 
+    an = zn; 
+end 
+
 
 % Loop through all of the image files 
-for k = 1:n 
+for k = 1:zn 
     % Store the current filename 
-    filename = strcat(image_path{1}, image_files{1,k});
+    filename = strcat(zline_path{1}, zline_images{1,k});
     
     % Perform the analysis including saving the image 
     im_struct = analyzeImage( filename, settings ); 
@@ -38,12 +74,12 @@ for k = 1:n
 
         if k == 1
             %Create a cell to store all distances 
-            all_lengths = cell(1,n); 
-            all_medians = cell(1,n); 
+            all_lengths = cell(1,zn); 
+            all_medians = cell(1,zn); 
             
             %If there is more than one image being analyzed, create a summary
             %file 
-            if n > 1
+            if zn > 1
                 %Get today's date in string form.
                 date_format = 'yyyy_mm_dd';
                 today_date = datestr(now,date_format);
@@ -53,7 +89,7 @@ for k = 1:n
                     '_zline_summary.mat');
 
                 %Save and create a summary file
-                save(fullfile(image_path{1}, summary_file_name), ...
+                save(fullfile(zline_path{1}, summary_file_name), ...
                     'all_lengths', 'all_medians', 'image_files');
             end 
         end 
@@ -78,9 +114,9 @@ for k = 1:n
         saveas(gcf, fullfile(im_struct.save_path, fig_name), 'tiffn');
         
         %If there is more than one FOV, save a summary file
-        if n > 1 
+        if zn > 1 
             %Append the summary file 
-            save(fullfile(image_path{1}, summary_file_name), 'all_lengths', ...
+            save(fullfile(zline_path{1}, summary_file_name), 'all_lengths', ...
                 'all_medians','-append');
         end 
         
