9afd18abd31b8e0d45333c37519b37e34971d038
tessamorris <tessam@uci.edu>
2019-01-20 14:28:57 -0800

    Create new function to calculate orientation vectors of actin

---
+++ functions/actinDetection.m
@@ -0,0 +1,42 @@
+% ACTINDETECTION - automaticly detect actin alignment in cardiomyocytes
+% stained with phalloidin by filtering and calculation oreintation vectors
+%
+% Arguments:
+%             Images of phaloidin stained actin captured from the
+%               same cover slip under the same exact aquisition conditions,
+%               file should TIF format grayscale at least 8-bit depth
+%
+% Returns:    Binary skeleton images of restored sarcomeres for each image
+%
+%             Binary skeleton images of restored actin after ROI based
+%               removal of non-sarcomeres for each image
+%
+%             *.actinOrientation.mat file containing actin alignment
+% Adapted from:
+% Peter Kovesi
+% School of Computer Science & Software Engineering
+% The University of Western Australia
+% pk at csse uwa edu au
+% http://www.csse.uwa.edu.au/~pk
+% January 2005
+%
+% and Adam W. Feinberg
+% Disease Biophysics Group
+% School of Engineering and Applied Sciences
+% Havard University, Cambridge, MA 02138
+
+% Last updated Sept 25, 2018 by Tessa Morris
+% Last updated May 20, 2014 by Anna Grosberg
+% The Edwards Lifesciences Center for Advanced Cardiovascular Technology
+% 2418 Engineering Hall
+% University of California, Irvine
+% Irvine, CA  92697-2700
+
+
+function [ output_args ] = actinDetection( input_args )
+%UNTITLED7 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
