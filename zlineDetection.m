f5247610781e63ea3db9188228e676cc59abbddb
tessamorris <tessam@uci.edu>
2019-01-20 12:01:03 -0800

    Add options to filter with actin

--- zlineDetection.m
+++ zlineDetection.m
@@ -22,7 +22,7 @@ function varargout = zlineDetection(varargin)
 
 % Edit the above text to modify the response to help zlineDetection
 
-% Last Modified by GUIDE v2.5 01-Oct-2018 09:56:49
+% Last Modified by GUIDE v2.5 20-Jan-2019 12:00:33
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -567,3 +567,81 @@ set( handles.tophat_size, 'String', num2str( settings.tophat_size ) );
 set( handles.noise_area, 'String', num2str( settings.noise_area ) ); 
 %>> Skeletonization Parameters
 set( handles.branch_size, 'String', num2str( settings.branch_size ) ); 
+
+
+
+function grid1_Callback(hObject, eventdata, handles)
+% hObject    handle to grid1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of grid1 as text
+%        str2double(get(hObject,'String')) returns contents of grid1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function grid1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to grid1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit19_Callback(hObject, eventdata, handles)
+% hObject    handle to edit19 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit19 as text
+%        str2double(get(hObject,'String')) returns contents of edit19 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit19_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit19 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function actin_thresh_Callback(hObject, eventdata, handles)
+% hObject    handle to actin_thresh (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of actin_thresh as text
+%        str2double(get(hObject,'String')) returns contents of actin_thresh as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function actin_thresh_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to actin_thresh (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in disp_actin.
+function disp_actin_Callback(hObject, eventdata, handles)
+% hObject    handle to disp_actin (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of disp_actin
