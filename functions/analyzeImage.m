e95e7d5f0252255a1d8988c84daf77c4b957f80b
tessamorris <tessam@uci.edu>
2019-01-20 22:37:26 -0800

    Maybe mess with actin filtering

--- functions/analyzeImage.m
+++ functions/analyzeImage.m
@@ -47,11 +47,47 @@ im_struct.save_path = fullfile(im_struct.im_path, new_subfolder);
 % Update user 
 disp('Filtering and computing orientation information...');
 
-% Fitler the image using coherence-enhancing anisotropic diffusion 
-% filtering and top hat filtering, then compute the orientation vectors 
-[ im_struct.gray, im_struct.CEDgray, im_struct.CEDtophat, ...
-    im_struct.orientim, im_struct.reliability ] = ...
-    orientInfo( im_struct.img, settings.Options, settings.tophat_size); 
+%Create a grayscale version of the image (if it was not already in
+%grayscale) 
+[ im_struct.gray ] = makeGray( im_struct.img); 
+
+% Run Diffusion Filter:
+% Coherence-Enhancing Anisotropic Diffusion Filtering, which enhances
+% contrast and calculates the orientation vectors for later usage. 
+% The parameters (supplied by the GUI) are (1) Orientation Smoothing and
+% (2) Diffusion Time 
+
+% Inputs are the grayscale image and the Options struct from settings. 
+% The output is the diffusion filtered image and eigenvectors - Not sure
+% why this is important, but... 
+[ CEDgray, ~, ~ ] = CoherenceFilter( im_struct.gray, settings.Options );
+
+% Clear the command line 
+clc; 
+
+% Convert the matrix to be an intensity image 
+CEDgray = mat2gray( CEDgray );
+
+% Run Top Hat Filter:
+%Compute the top hat filter using the disk structuring element with the
+%threshold defined by the user input tophat filter. It then adjusts the
+%image so that 1% of data is saturated at low and high intensities of the
+%image 
+CEDtophat = imadjust( imtophat( CEDgray, strel( 'disk', ...
+    tophat_size ) ) );
+
+% Calculate orientation vectors
+[orientim, reliability] = ridgeorient(CEDtophat, ...
+    Options.sigma, Options.rho, Options.rho);
+
+
+
+
+% % Fitler the image using coherence-enhancing anisotropic diffusion 
+% % filtering and top hat filtering, then compute the orientation vectors 
+% [ im_struct.gray, im_struct.CEDgray, im_struct.CEDtophat, ...
+%     im_struct.orientim, im_struct.reliability ] = ...
+%     orientInfo( im_struct.img, settings.Options, settings.tophat_size); 
 
 % If the user would like to display the diffusion filtered image, display 
 % it
