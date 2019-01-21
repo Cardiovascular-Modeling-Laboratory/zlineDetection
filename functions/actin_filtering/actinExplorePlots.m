d839ab29ca977f64ef848d1e8c779f5a20d5751f
tessamorris <tessam@uci.edu>
2019-01-21 13:22:53 -0800

    Needed to add hold on to each sub plot individually

--- functions/actin_filtering/actinExplorePlots.m
+++ functions/actin_filtering/actinExplorePlots.m
@@ -1,4 +1,4 @@
-function [ ] = actinExplorePlots( summary_explore, actin_explore, settings )
+function [ ] = actinExplorePlots( im_struct, summary_explore, actin_explore, settings )
 
 if settings.actin_thresh > 1 && settings.grid_explore
     
@@ -17,12 +17,13 @@ if settings.actin_thresh > 1 && settings.grid_explore
         actin_thresh = summary_explore.actin_thresh{k,1}; 
         medians = summary_explore.medians{k,1}; 
         sums = summary_explore.sums{k,1}; 
-        non_sarcs = summary_explore.non_sarcs{k,1}; 
+        non_sarcs = summary_explore.non_sarcs{k,1};
+        grid = summary_explore.grid_sizes(k,1); 
         
         %Plot the threshold vs. the median
         subplot(3,2,1); 
-        plot(actin_thresh, medians, ...
-            'o', 'color', c(k),  'MarkerFaceColor', c(k)); 
+        hold on; 
+        plot(actin_thresh, medians, 'o', 'color', c{k},'MarkerFaceColor', c{k}); 
         set(gca,'fontsize',12)
         title( strcat('Median (\mu m) vs Actin Threshold'),...
             'FontSize',12,'FontWeight','bold');
@@ -32,8 +33,9 @@ if settings.actin_thresh > 1 && settings.grid_explore
 
         %Plot the threshold vs. the sum
         subplot(3,2,2); 
+        hold on; 
         plot(actin_thresh, sums, ...
-            'o', 'color', c(k),  'MarkerFaceColor', c(k)); 
+            'o', 'color', c{k},  'MarkerFaceColor', c{k}); 
         set(gca,'fontsize',12)
         title( strcat('Sum (\mu m) vs Actin Threshold'),...
             'FontSize',12,'FontWeight','bold');
@@ -43,8 +45,9 @@ if settings.actin_thresh > 1 && settings.grid_explore
 
         %Plot the non-zline fraction vs. actin trheshold 
         subplot(3,2,3); 
+        hold on; 
         plot(actin_thresh, non_sarcs, ...
-            'o', 'color', c(k),  'MarkerFaceColor', c(k)); 
+            'o', 'color', c{k},  'MarkerFaceColor', c{k}); 
         set(gca,'fontsize',12)
         title( strcat('Non Z-line Fraction vs Actin Threshold'),...
             'FontSize',12,'FontWeight','bold');
@@ -54,8 +57,9 @@ if settings.actin_thresh > 1 && settings.grid_explore
 
         %Plot the median vs non zline 
         subplot(3,2,4); 
+        hold on; 
         plot(non_sarcs, medians, ...
-            'o', 'color', c(k),  'MarkerFaceColor', c(k)); 
+            'o', 'color', c{k},  'MarkerFaceColor', c{k}); 
         set(gca,'fontsize',12)
         title( strcat('Median (\mu m) vs Non Z-line Fraction'),...
             'FontSize',12,'FontWeight','bold');
@@ -65,8 +69,9 @@ if settings.actin_thresh > 1 && settings.grid_explore
 
         %Plot the sum vs non zline 
         subplot(3,2,5); 
+        hold on; 
         plot(non_sarcs, sums,...
-                'o', 'color', c(k),  'MarkerFaceColor', c(k)); 
+                'o', 'color', c{k},  'MarkerFaceColor', c{k}); 
         set(gca,'fontsize',12)
         title( strcat('Sum (\mu m) vs Non Z-line Fraction'),...
             'FontSize',12,'FontWeight','bold');
@@ -76,21 +81,23 @@ if settings.actin_thresh > 1 && settings.grid_explore
         
         
         %Plot the grid sizes in the correct color
-        subplot(3,2,5); 
-        plot(summary_explore.grid_sizes, summary_explore.grid_sizes,...
-                'o', 'color', c(k),  'MarkerFaceColor', c(k)); 
+        subplot(3,2,6); 
+        hold on; 
+        plot(grid, grid,...
+                'o', 'color', c{k},  'MarkerFaceColor', c{k}); 
         set(gca,'fontsize',12)
         title( strcat('Simple Legend'),...
             'FontSize',12,'FontWeight','bold');
         ylabel('Grid Size (pixels)','FontSize',12,...
             'FontWeight','bold');
         xlabel('Grid Size (pixels)','FontSize',12,'FontWeight','bold');
+
+    end
+    %Save pdf
+    saveas(gcf, fullfile(im_struct.save_path, ...
+        strcat(im_struct.im_name, '_ActinExplorationSummary')), 'pdf');
         
-        %Save pdf
-        saveas(gcf, fullfile(im_struct.save_path, ...
-            strcat(im_struct.im_name, '_ActinExplorationSummary')), 'pdf');
-    end 
-end 
+end
     
 end
 
