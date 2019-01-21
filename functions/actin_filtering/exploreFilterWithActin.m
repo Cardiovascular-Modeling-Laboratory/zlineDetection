de061f22049edc1a817a4f5285c4afa8d764b6dd
tessamorris <tessam@uci.edu>
2019-01-20 19:33:07 -0800

    Modify parameter exploration - need to add continuous z-line calc

--- functions/actin_filtering/exploreFilterWithActin.m
+++ functions/actin_filtering/exploreFilterWithActin.m
@@ -12,51 +12,112 @@ new_subfolder = ...
 % Save the name of the new path 
 actin_explore.save_path = fullfile(im_struct.im_path, new_subfolder); 
 
+
+%Create a cell to store all of the masks
+actin_explore.masks = ...
+    cell(length(actin_explore.min_thresh:actin_explore.thresh_step:...
+    actin_explore.max_thresh), 1); 
+
+%Create a cell to store all of the final skeletons
+actin_explore.final_skels = ...
+    cell(length(actin_explore.min_thresh:actin_explore.thresh_step:...
+    actin_explore.max_thresh), 1); 
+
+%Create a cell to store all of the orientation matrices
+actin_explore.orientims = ...
+    cell(length(actin_explore.min_thresh:actin_explore.thresh_step:...
+    actin_explore.max_thresh), 1); 
+
+%Create a cell to store all of the continuous z-line lengths
+actin_explore.lengths = ...
+    cell(length(actin_explore.min_thresh:actin_explore.thresh_step:...
+    actin_explore.max_thresh), 1); 
+
+%Create a matrix to store all of the medians 
+actin_explore.medians = zeros(length(actin_explore.min_thresh:...
+    actin_explore.thresh_step:...
+    actin_explore.max_thresh), 1); 
+
+%Create a matrix to store all of the non-sarc percentages 
+actin_explore.non_sarcs = zeros(length(actin_explore.min_thresh:...
+    actin_explore.thresh_step:...
+    actin_explore.max_thresh), 1); 
+
+
+% Start a counter 
+actin_explore.n = 0; 
+
+% Save the pre-filtered skeleton for calculation of the non-sarc
+% percentages
+pre_filt = im_struct.skel_final; 
+pre_filt = pre_filt(:); 
+pre_filt(pre_filt == 0) = []; 
+
+% Save the data (append on each iteration)
+save(fullfile(actin_explore.save_path, strcat(im_struct.im_name,...
+    '_ActinExploration.mat')), 'im_struct', 'settings', 'actin_explore');
+
+for thresh = actin_explore.min_thresh:actin_explore.thresh_step:...
+        actin_explore.max_thresh
     
-%Total number of grids
-n = size(director,1); 
-
-%Create mask 
-mask = ones(size(orientim)); 
-
-%Create a direcor matrix
-dir_mat = ones(size(orientim)); 
-
-%Check to make sure the direcor isn't in radians
-if max(director) > 10
-    director = deg2rad(director); 
-end 
-
-%Loop through each grid and place the director of each grid as the value in
-%the grid
-for k = 1:n
-    %Set the value each grid equal to its director
-    dir_mat(dims(k,1):dims(k,2),dims(k,3):dims(k,4)) = director(k,1);     
-end 
-
-%Take the dot product sqrt(cos(th1 - th2)^2);
-dp = sqrt(cos(orientim - dir_mat).^2); 
-
-for thresh = 0:0.05:1
+    %Create a temporary save name 
+    save_name = strcat(im_struct.im_name, '_ACTINthresh', ...
+        num2str(actin_explore.n));
+    
+    %Increase counter 
+    actin_explore.n = actin_explore.n+1; 
+    
+    %Create a matrix to store the mask 
+    mask = ones(size(im_struct.orientim)); 
+    
     %If dot product is closer to 1, the angles are more parallel and should be
     %removed
-    mask(dp >= thresh) = 0; 
+    mask(im_struct.dp >= thresh) = 0; 
     %If dot product is closer to 0, the angles are more perpendicular and
     %should be kept
-    mask(dp < thresh) = 1; 
+    mask(im_struct.dp < thresh) = 1; 
 
     %The NaN postitions should be set equal to 1 (meaning no director for
     %actin)
     mask(isnan(mask)) = 1; 
 
-    %Create a skeleton by setting every positive pixel in the orientation
-    %matrix equal to 1
-    final_skel = orientim; 
-    final_skel(~isnan(orientim)) = 1; 
-    final_skel(isnan(orientim)) = 0;
-    %Multiply the final skeleton by the mask (removing points below the
-    %threshold
-    final_skel = final_skel.*mask; 
+    %Store the mask in the masks cell
+    actin_explore.masks{actin_explore.n,1} = mask; 
+    
+    %Modify the final skeleton by multiplying by the maks 
+    actin_explore.final_skels{actin_explore.n,1} = ...
+        im_struct.skel_final.*mask; 
+    
+    % Remove regions that were not part of the binary skeleton
+    temp_orientim = im_struct.orientim; 
+    temp_orientim(~actin_explore.final_skels{actin_explore.n,1}) = NaN; 
+    actin_explore.orientims{actin_explore.n,1} = temp_orientim; 
+    
+    
+    % Save the mask. 
+    imwrite( mask, fullfile(actin_explore.save_path, ...
+        strcat( save_name, '_Mask.tif' ) ),...
+        'Compression','none');
+
+    % Save the final skeleton. 
+    imwrite( actin_explore.final_skels{actin_explore.n,1}, ...
+        fullfile(actin_explore.save_path, ...
+        strcat( save_name, '_Skeleton.tif' ) ),...
+        'Compression','none');
+    
+    % Isolate the number of pixels in the post filtering skeleton 
+    post_filt = actin_explore.final_skels{actin_explore.n,1};
+    post_filt = post_filt(:);
+    post_filt(post_filt == 0) = []; 
+    
+    % Calculate the non-sarcomeric alpha actinin 
+    % number of pixles eliminated / # total # of pixles positive for alpha
+    % actinin 
+    actin_explore.non_sarcs(actin_explore.n,1) = ...
+        (length(pre_filt) - length(post_filt))/ ...
+        length(pre_filt);
+    
+    % Calculate the continuous z-line lengths 
     
     %If analyzing continuous z-line length add folder to path 
     if czl
