845d13091094a47b0959e2492e474e132aee5101
tessamorris <tessam@uci.edu>
2019-02-02 15:59:03 -0800

    Fix plots to be compatiable with new data storage Now getting rid of the unncessary extra .mat files

--- functions/actin_filtering/actinExplorePlots.m
+++ functions/actin_filtering/actinExplorePlots.m
@@ -1,104 +1,115 @@
-function [ ] = actinExplorePlots( im_struct, summary_explore, actin_explore, settings )
+function [ ] = actinExplorePlots( im_struct, actin_explore, settings )
 
-if settings.actin_thresh > 1 && settings.grid_explore
+%Get unique threshold values 
+unique_thresh = unique(actin_explore.thresholds); 
+
+%Get unique grid sizes
+unique_grids = unique(actin_explore.grid_sizes); 
+gtot = length(unique_grids); 
+
+%Declare colors 
+c = {'g','c','r','b','m','y'};
+
+%Open a figure and hold on 
+figure; 
+hold on; 
+
+%Loop through all of the grids 
+for g= 1:gtot
     
-    %Check how many grid conditions there are
-    test = summary_explore.non_sarcs;
-    cond = size(test,1); 
+    %Set up the grids to exclude 
+    exclude_grid = zeros(size(actin_explore.grid_sizes)); 
     
-    %Open a figure and hold on 
-    figure; 
+    %Set the values that are not equal to the current grid size equal to
+    %NaN. Otherwise, set the grid size to 1 
+    exclude_grid(grid_sizes ~= unique_grids(g)) = NaN; 
+    exclude_grid(~isnan(exclude_grid)) = 1; 
+    
+    % Get the included values 
+    included_sums = actin_explore.sums + exclude_grid;
+    included_sums(isnan(included_sums)) = []; 
+    included_medians = actin_explore.medians + exclude_grid;
+    included_medians(isnan(included_medians)) = [];
+    included_actinthresh = actin_explore.thresholds + exclude_grid;
+    included_actinthresh(isnan(included_actinthresh)) = [];
+    included_nonsarc = actin_explore.non_sarcs + exclude_grid; 
+    included_nonsarc(isnan(included_nonsarc)) = [];
+    
+    %Plot the threshold vs. the median
     hold on; 
-    
-    %Define colors 
-    c = {'g','c','r','b','m','y'}; 
-    
-    for k = 1:cond
-        actin_thresh = summary_explore.actin_thresh{k,1}; 
-        medians = summary_explore.medians{k,1}; 
-        sums = summary_explore.sums{k,1}; 
-        non_sarcs = summary_explore.non_sarcs{k,1};
-        grid = summary_explore.grid_sizes(k,1); 
-        
-        %Plot the threshold vs. the median
-        subplot(3,2,1); 
-        hold on; 
-        plot(actin_thresh, medians, 'o', 'color', c{k}); 
-        set(gca,'fontsize',12)
-        title( strcat('Median (\mu m) vs Actin Threshold'),...
-            'FontSize',12,'FontWeight','bold');
-        ylabel('Median CZL (\mu m)','FontSize',12,...
-            'FontWeight','bold');
-        xlabel('Actin Threshold (dot product)','FontSize',12,'FontWeight','bold');
+    plot(included_actinthresh, included_medians, 'o', 'color', c{g}); 
+    set(gca,'fontsize',12)
+    title( strcat('Median (\mu m) vs Actin Threshold'),...
+        'FontSize',12,'FontWeight','bold');
+    ylabel('Median CZL (\mu m)','FontSize',12,...
+        'FontWeight','bold');
+    xlabel('Actin Threshold (dot product)','FontSize',12,'FontWeight','bold');
 
-        %Plot the threshold vs. the sum
-        subplot(3,2,2); 
-        hold on; 
-        plot(actin_thresh, sums,'o', 'color', c{k}); 
-        set(gca,'fontsize',12)
-        title( strcat('Sum (\mu m) vs Actin Threshold'),...
-            'FontSize',12,'FontWeight','bold');
-        ylabel('Total CZL (\mu m)','FontSize',12,...
-            'FontWeight','bold');
-        xlabel('Actin Threshold (dot product)','FontSize',12,'FontWeight','bold');
+    %Plot the threshold vs. the sum
+    subplot(3,2,2); 
+    hold on; 
+    plot(included_actinthresh, included_sums,'o', 'color', c{g}); 
+    set(gca,'fontsize',12)
+    title( strcat('Sum (\mu m) vs Actin Threshold'),...
+        'FontSize',12,'FontWeight','bold');
+    ylabel('Total CZL (\mu m)','FontSize',12,...
+        'FontWeight','bold');
+    xlabel('Actin Threshold (dot product)','FontSize',12,'FontWeight','bold');
 
-        %Plot the non-zline fraction vs. actin trheshold 
-        subplot(3,2,3); 
-        hold on; 
-        plot(actin_thresh, non_sarcs,'o', 'color', c{k}); 
-        set(gca,'fontsize',12)
-        title( strcat('Non Z-line Fraction vs Actin Threshold'),...
-            'FontSize',12,'FontWeight','bold');
-        ylabel('Non Z-line Fraction','FontSize',12,...
-            'FontWeight','bold');
-        xlabel('Actin Threshold (dot product)','FontSize',12,'FontWeight','bold');
+    %Plot the non-zline fraction vs. actin trheshold 
+    subplot(3,2,3); 
+    hold on; 
+    plot(included_actinthresh, included_nonsarc,'o', 'color', c{g}); 
+    set(gca,'fontsize',12)
+    title( strcat('Non Z-line Fraction vs Actin Threshold'),...
+        'FontSize',12,'FontWeight','bold');
+    ylabel('Non Z-line Fraction','FontSize',12,...
+        'FontWeight','bold');
+    xlabel('Actin Threshold (dot product)','FontSize',12,'FontWeight','bold');
 
-        %Plot the median vs non zline 
-        subplot(3,2,4); 
-        hold on; 
-        plot(non_sarcs, medians,'o', 'color', c{k}); 
-        set(gca,'fontsize',12)
-        title( strcat('Median (\mu m) vs Non Z-line Fraction'),...
-            'FontSize',12,'FontWeight','bold');
-        ylabel('Median CZL (\mu m)','FontSize',12,...
-            'FontWeight','bold');
-        xlabel('Non Z-line Fraction','FontSize',12,'FontWeight','bold');
+    %Plot the median vs non zline 
+    subplot(3,2,4); 
+    hold on; 
+    plot(included_nonsarc, included_medians,'o', 'color', c{g}); 
+    set(gca,'fontsize',12)
+    title( strcat('Median (\mu m) vs Non Z-line Fraction'),...
+        'FontSize',12,'FontWeight','bold');
+    ylabel('Median CZL (\mu m)','FontSize',12,...
+        'FontWeight','bold');
+    xlabel('Non Z-line Fraction','FontSize',12,'FontWeight','bold');
 
-        %Plot the sum vs non zline 
-        subplot(3,2,5); 
-        hold on; 
-        plot(non_sarcs, sums,'o', 'color', c{k}); 
-        set(gca,'fontsize',12)
-        title( strcat('Sum (\mu m) vs Non Z-line Fraction'),...
-            'FontSize',12,'FontWeight','bold');
-        ylabel('Total CZL (\mu m)','FontSize',12,...
-            'FontWeight','bold');
-        xlabel('Non Z-line Fraction','FontSize',12,'FontWeight','bold');
+    %Plot the sum vs non zline 
+    subplot(3,2,5); 
+    hold on; 
+    plot(included_nonsarc, included_sums,'o', 'color', c{g}); 
+    set(gca,'fontsize',12)
+    title( strcat('Sum (\mu m) vs Non Z-line Fraction'),...
+        'FontSize',12,'FontWeight','bold');
+    ylabel('Total CZL (\mu m)','FontSize',12,...
+        'FontWeight','bold');
+    xlabel('Non Z-line Fraction','FontSize',12,'FontWeight','bold');
         
         
-        %Plot the grid sizes in the correct color
-        subplot(3,2,6); 
-        hold on; 
-        plot(grid, 1, 'o', 'color', c{k},  'MarkerFaceColor', c{k}); 
-        set(gca,'fontsize',12)
-        title( strcat('Simple Legend'),...
-            'FontSize',12,'FontWeight','bold');
-%         ylabel('Grid Size (pixels)','FontSize',12,...
-%             'FontWeight','bold');
-        xlabel('Grid Size (pixels)','FontSize',12,'FontWeight','bold');
-        ylim([0 2])
-        xlim([min(summary_explore.grid_sizes(:))-1 ...
-            max(summary_explore.grid_sizes(:))+1])
-        set(gca,'ytick',[])
-    end
-    
-    %Change last plot 
-    
-    %Save pdf
-    saveas(gcf, fullfile(im_struct.save_path, ...
-        strcat(im_struct.im_name, '_ActinExplorationSummary')), 'pdf');
-        
+    %Plot the grid sizes in the correct color
+    subplot(3,2,6); 
+    hold on; 
+    plot(unique_grids(g), 1, 'o', 'color', c{g}, ...
+        'MarkerFaceColor', c{g}); 
+    set(gca,'fontsize',12)
+    title( strcat('Legend'),...
+        'FontSize',12,'FontWeight','bold');
+
+    xlabel('Grid Size (pixels)','FontSize',12,'FontWeight','bold');
+    ylim([0 2])
+    xlim([min(summary_explore.grid_sizes(:))-1 ...
+        max(summary_explore.grid_sizes(:))+1])
+    set(gca,'ytick',[])
 end
+    
+    
+%Save pdf
+saveas(gcf, fullfile(im_struct.save_path, ...
+    strcat(im_struct.im_name, '_ActinExplorationSummary')), 'pdf');
     
 end
 
