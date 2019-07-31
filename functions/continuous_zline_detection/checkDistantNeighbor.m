f6b5ade75dd63c2a52916c7e6a44ead0d3ab7af4
tessamorris <tessam@uci.edu>
2019-07-30 17:23:21 -0700

    Compare o angles for neighbors Still need to do for mulitple clusters

--- functions/continuous_zline_detection/checkDistantNeighbor.m
+++ functions/continuous_zline_detection/checkDistantNeighbor.m
@@ -1,14 +1,6 @@
-function [temp_cluster, cluster1, cluster2, shouldIgnore] = ...
+function [id_dp, dps] = ...
     checkDistantNeighbor(temp_cluster, ...
     atTop, dp_thresh, angles, cluster1, cluster2)
-
-% Get the size of the current cluster and the temp_cluster
-[~, temp_num ] = size(temp_cluster); 
-
-% Possibilities 
-% temp_num = 2 ( d0 and d1/d2)
-% temp_num = 1 ( d0/d1/d2 )
-% temp_num = 0 ( combining two clusters ) 
 
 % Check if there are one or two clusters 
 if nargin < 6
@@ -16,6 +8,9 @@ if nargin < 6
     oneCluster = true; 
     cluster2 = NaN; 
 end 
+
+% Get the size of the current cluster and the temp_cluster
+[~, temp_num ] = size(temp_cluster); 
 
 % Initialize shouldIgnore
 shouldIgnore = false; 
@@ -34,6 +29,9 @@ if c1_num < 2
     shouldIgnore = true; 
 end 
 
+
+
+
 % If there is only one cluster and there is no reason to ignore the
 % temporary cluster, find the positions of the neighbors 
 if  oneCluster && ~shouldIgnore 
@@ -43,64 +41,83 @@ if  oneCluster && ~shouldIgnore
         away1_temp = 2; 
         close_cluster1 = c1_num-1; 
         away1_cluster1 = c1_num; 
-        disp('Not at top'); 
-
     else
         % Find the positions of the closest and one away positions 
         close_temp = temp_num; 
         away1_temp = temp_num - 1; 
         close_cluster1 = 1; 
         away1_cluster1 = 2; 
-        disp('Not at bottom'); 
     end
     
-    % First compare the close temp theta to away cluster1 theta
+    % Initialize a vector to store the dot products between groups
+    % This is dependent on the size of the temporary cluster 
+    if temp_num > 1
+        nc = 4;
+    else
+        nc = 1; 
+    end 
+    
+    % The identifying information will be a 2xnc vector that contains the
+    % (1) identifying cluster 0-temp 1-cluster1, 2-cluster2 and (2) the
+    % position in that cluster (3) identifying cluster (4) the position in
+    % that cluster 
+    id_dp = zeros(4,nc); 
+    dps = zeros(1,nc); 
+    
+    
     theta_ct = angles(temp_cluster(1,close_temp), ...
         temp_cluster(1,close_temp)); 
     theta_a1c1 = angles(cluster1(1,away1_cluster1), ...
         cluster1(1,away1_cluster1)); 
     
-    % Compare the angles. If they're perpendicular, do not add to cluster
-    if sqrt(cos(theta_ct-theta_a1c1)^2) > dp_thresh 
-        disp('Close Parallel'); 
-        % If there is only one value in the temp cluster add to the cluster
-        % as is. If not, compare the next  
-        if temp_num > 1
-            disp('More than one temp'); 
-            theta_a1t = angles(temp_cluster(1,away1_temp), ...
-                temp_cluster(1,away1_temp)); 
-            theta_cc1 = angles(cluster1(1,close_cluster1), ...
-                cluster1(1,close_cluster1)); 
-            
-            % If they're perpendicular, decide what to do with the
-            % temporary cluster based which values are closer 
-            if sqrt(cos(theta_a1t-theta_cc1)^2) <= dp_thresh 
-                disp('Further Perpendicular'); 
-                % Determine which orinetation angle the closest temp is
-                % more parallel to 
-                if sqrt(cos(theta_ct-theta_a1t)^2) >= ...
-                        sqrt(cos(theta_ct-theta_cc1)^2)
-                    disp('Temp closer to temp'); 
-                    shouldIgnore = true; 
-                else
-                    % Change the temporary cluster to only be the closest
-                    % dp
-                    place_temp = temp_cluster; 
-                    clear temp_cluster 
-                    temp_cluster = place_temp(:,theta_ct); 
-                    disp('Temp closer to cluster'); 
-                end 
-            end 
-            
-        end 
-    else
-        disp('Perp'); 
-        % Do not add anything to closer 
-        shouldIgnore = true; 
-        
-    end 
+    % Identify the cluster and corresponding positions in the id matrix 
+    id_dp(1,1) = 0; 
+    id_dp(3,1) = 1; 
+    id_dp(2,1) = close_temp; 
+    id_dp(4,1) = away1_cluster1; 
     
+    % Calculate the dot product 
+    dps(1,1) =sqrt(cos(theta_ct-theta_a1c1)^2);  
+    
+    % If there is more than one vector in teh temporary array, compute the
+    % dot products 
+    if temp_num > 1
+        % Look up the oreientation vectors
+        theta_a1t = angles(temp_cluster(1,away1_temp), ...
+            temp_cluster(1,away1_temp)); 
+        theta_cc1 = angles(cluster1(1,close_cluster1), ...
+                cluster1(1,close_cluster1));
+            
+        % Identify the cluster
+        id_dp(1,2) = 0; 
+        id_dp(3,2) = 1; 
+        % Corresponding positions in the id matrix 
+        id_dp(2,2) = away1_temp; 
+        id_dp(4,2) = close_cluster1;       
+        % Compute the dot product. 
+        dps(1,2) = sqrt(cos(theta_a1t-theta_cc1)^2); 
+        
+        % Identify the cluster
+        id_dp(1,3) = 0; 
+        id_dp(3,3) = 0; 
+        % Corresponding positions in the id matrix 
+        id_dp(2,3) = close_temp; 
+        id_dp(4,3) = away1_temp; 
+        % Compare the close temp theta value with the far temp theta value
+        dps(1,3) = sqrt(cos(theta_ct-theta_a1t)^2);
+        
+        % Identify the cluster
+        id_dp(1,4) = 0; 
+        id_dp(3,4) = 1; 
+        % Corresponding positions in the id matrix 
+        id_dp(2,4) = close_temp; 
+        id_dp(4,4) = away1_cluster1; 
+        % Compare the close temp theta value with the close cluster 1 
+        % theta value
+        dps(1,4) = sqrt(cos(theta_ct-theta_cc1)^2); 
+                    
+    end 
 end 
-
-end
+        
+end 
 
