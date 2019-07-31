6c11fcc90d7b230a7ac22ac58e3b396732211082
tessamorris <tessam@uci.edu>
2019-07-31 14:08:27 -0700

    Write CZL function for zlineDetection Now need to make sure I incorporate it everywhere that is necessary

--- functions/continuous_zline_detection/continuous_zline_detection.m
+++ functions/continuous_zline_detection/continuous_zline_detection.m
@@ -1,60 +1,72 @@
-function [ distances_um ] = continuous_zline_detection(im_struct, settings)
-% This method aims to detect the continuous lines in an image of alpha 
-% actinin stained sarcomeres. 
+% continuous_zline_detection - detect the continuous lines in an image of 
+% alpha - actinin stained z-lines in images of striated muscle tissues. 
+%
+% Usage:   
+%   [ distances_um ] = continuous_zline_detection(angles, BW, ...
+%       dot_product_error, pix2um)
+%
+% Arguments:
+%   angles       - orientation angles in radians with no NaN values
+%                           Class Support: numeric 
+%   BW          - image 
+%   dot_product_error
+% vpix2um
+% Returns:
+%   OOP                 - orientational order parameters
+%                           Class Support: double 
+%	directionAngle      - principle direction angle in degrees 
+%                           Class Support: double 
+%   direction_error     - difference between principle direction (in 
+%                               degrees) average direction (in degrees)
+%                           Class Support: double 
+%   director            - principle direction vector
+%                           Class Support: 2x1 double 
+%
+% Dependencies: 
+%   MATLAB Version >= 9.5 
+%
+%
+% Written by: Tessa Morris
+%   Advisor: Anna (Anya) Grosberg, Department of Biomedical Engineering 
+%   Cardiovascular Modeling Laboratory 
+%   University of California, Irvine 
 
-%%%%%%%%%%%%%%%%%%%%%%%% LOAD FROM IM_STRUCT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Check to see if this is a parameter exploration for actin filtering
-if ~settings.actinthresh_explore && ~settings.grid_explore
-    %Load orientation angles from the image structure
-    angles = im_struct.orientim; 
+function [ CZL_results, CZL_info ] = continuous_zline_detection( angles, BW, ...
+    dot_product_error, save_info )
 
-%     %Create a .mat filename 
-%     output_filename = strcat( im_struct.im_name, '_zlines.mat'); 
-    output_filename = im_struct.im_name; 
-    %Store the location to save all of the files
-    save_path = im_struct.save_path; 
-else 
-    %Save the actin explore struct 
-    actin_explore = im_struct.actin_explore; 
+% If there are only three inputs, the user does not want to save
+if nargin < 4
+    % Set save results to false
+    saveResults = false;
+else
+    % Set save results to true and store the image info  
+    saveResults = true;
     
-    %Load orientation angles from the current iteration of the
-    %actin_explore struct
-    angles = actin_explore.orientims{actin_explore.n,1};
-    
-    %Create a .mat filename (only save the threshold if this is a threshold
-    %exploration 
-    if settings.actinthresh_explore
-        output_filename = strcat( im_struct.im_name,'_ACTINthresh', ...
-            num2str(actin_explore.n)); 
-    else
-        output_filename = strcat( im_struct.im_name ); 
-  
+    % Make sure the user provided a save name and save path 
+    if ~isfield(save_info,'save_path')
+        disp('You must proide a path to save the results.'); 
+        disp('save_info.save_path = ?'); 
+        saveResults = false; 
     end 
-    
-    %Store the location to save all of the files 
-    save_path = actin_explore.save_path; 
+    if ~isfield(save_info,'save_name') && ~saveResults
+        save_info.save_name = 'contZlineResults'; 
+    end 
+    % If the user did not provide an argument for saving as a figure, set
+    % it to false
+    if ~isfield(save_info,'saveFig') && ~saveResults
+        save_info.saveFig = false; 
+    end 
 end 
 
 %If there are any NaN values in the angles matrix, set them to 0.
 angles(isnan(angles)) = 0; 
 
-%Save the pixel to micron conversion
-pix2um = settings.pix2um; 
-
-%Save the dot prodcut error 
-dot_product_error = settings.dp_threshold; 
-
-%Save the image and convert to a matrix 
-BW = im_struct.im; 
-BW = mat2gray(BW);
-
 %Find the nonzero positions of this matrix and get their values. 
 [ nonzero_rows, nonzero_cols] = find(angles);
-    
-[ all_angles ] = get_values(nonzero_rows, nonzero_cols, ...
-    angles);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%% BEGIN CZL METHOD %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Get the value of the orientation angles at all of the non-zero positions 
+all_angles = get_values(nonzero_rows, nonzero_cols, ...
+    angles);
 
 %Find the boundaries of the edges
 [m,n] = size(angles);
@@ -75,106 +87,75 @@ BW = mat2gray(BW);
 [ dp_rows, dp_cols] = compare_angles( dot_product_error,...
     angles, nonzero_rows, nonzero_cols, corrected_rows, corrected_cols);
 
+% Remove any orientation vectors in which the neighbors are perpendicular 
+[  dp2_rows, dp2_cols ] = ...
+    check_perpendicular( dp_rows, dp_cols, angles, dp_thresh); 
+
 %Cluster the values in order.  
 [ zline_clusters, cluster_tracker ] = ...
-    cluster_neighbors( dot_product_error, angles, dp_rows, dp_cols);
+    cluster_neighbors( dot_product_error, angles, dp2_rows, dp2_cols);
 
 %Calculate legnths and plot
 disp('Plotting and calculating the lengths of continuous z-lines...'); 
 [ distance_storage, rmCount, zline_clusters ] = ...
     calculate_lengths( BW, zline_clusters);
 
-%If this is a actin filtering parameter exploration, add a title
-if settings.actinthresh_explore || settings.grid_explore
-    im_title = strcat(strrep(im_struct.im_name,'_', '\_'), ...
-        '; Actin Threshold: ', {' '}, ...
-        num2str(actin_explore.thresholds(actin_explore.n,1)),...
-        {' '},'; Grid Size: ',num2str(settings.grid_size(1))); 
-    
-    %Add title
-    title(im_title{1,1},'FontSize',14,'FontWeight','bold');
-end
-
-%Save as a .fig file (Matlab Figure)
-fig_name = strcat( output_filename, '_zlines.fig' );
-savefig(fullfile(save_path, fig_name));
-%Save as a .tif file
-saveas(gcf, fullfile(save_path, fig_name(1:end-4)), 'tiffn');
-
-
-settings.pltZact = false; 
-% If the actin detect image is available, plot the z-lines on top of it 
-if settings.pltZact 
-    % Save the actin struct
-    actin_struct = im_struct.actin_struct;
-    
-    %Save the actin image
-    actin_im = actin_struct.actin_im;
-    
-    %Convert to gray matrix
-    actin_im = mat2gray(actin_im); 
-    
-    %Plot z-lines on top of the actin image 
-    disp('Plotting continuous z-lines on actin image...'); 
-    [ ~, ~, ~ ] = ...
-        calculate_lengths( actin_im, zline_clusters);
-    
-    %Get the file parts of the actin 
-    [~, actin_name] = fileparts(actin_struct.filename);
-    disp(actin_struct.filename); 
-    %If this is a parameter exploration, add the parameter threshold number
-    if settings.actinthresh_explore || settings.grid_explore
-        
-        %Add title
-        im_title = strcat(strrep(actin_name,'_', '\_'), ...
-            '; Actin Threshold: ', {' '}, ...
-            num2str(actin_explore.actin_thresh(actin_explore.n,1)),...
-            {' '},'; Grid Size: ',num2str(settings.grid_size(1))); 
-        %Add title
-        title(im_title{1,1},'FontSize',14,'FontWeight','bold');
-
-        %Save the actin image as a .tif and .fig
-        actin_name = strcat(actin_name, '_ACTINthresh', ...
-            num2str(actin_explore.n),'_zlines');
+% If the results should be saved 
+if saveResults
+    % Add a title to the plot if the field exists 
+    if isfield(save_info,'plot_title')
+        title(save_info.plot_title,'FontSize',14,'FontWeight','bold');
     end 
+    % Save the figure 
+    if save_info.saveFig
+        %Save as a .fig file (Matlab Figure)
+        new_filename = appendFilename( save_info.save_path, ...
+            strcat(save_info.save_name, '.fig' ));
+        savefig(fullfile(save_info.save_path, new_filename));
+    end
     
-    %Add .fig extension to name 
-    actin_name = strcat(actin_name, '.fig'); 
-    
-    %Save the .fig and .tiff 
-    savefig(fullfile(save_path, actin_name));
-    saveas(gcf, fullfile(save_path, actin_name(1:end-4)),...
-        'tiffn');
+    % Save as an image if the file type was provided
+    if isfield(save_info,'save_type')
+        new_filename = appendFilename( save_info.save_path, ...
+            strcat(save_info.save_name,save_info.save_type) ); 
+        saveas(gcf, fullfile(save_info.save_path, new_filename),...
+            save_info.save_type);
+    end 
 end 
 
 %Remove any nan from distances 
 distances_no_nan = distance_storage; 
 distances_no_nan(isnan(distances_no_nan)) = []; 
 
-%Convert the distances from pixels to microns 
-distances_um = distances_no_nan/pix2um;
+% Store the results of the continuous z-line detection   
+CZL_results = struct(); 
+CZL_results.zline_clusters = zline_clusters;
+CZL_results.cluster_tracker = cluster_tracker;
+CZL_results.distance_storage = distance_storage;
+CZL_results.distances_no_nan = distances_no_nan;
+CZL_results.rmCount = rmCount;
 
-%Save information
-disp('Saving data...'); 
+% Store all of the intermediate steps. This is primarily used for
+% demonstrative/ debugging purposes 
+CZL_info = struct(); 
+CZL_info.angles = angles;
+CZL_info.dot_product_error = dot_product_error;
+CZL_info.candidate_rows = candidate_rows;
+CZL_info.candidate_cols = candidate_cols;
+CZL_info.corrected_rows = corrected_rows;
+CZL_info.corrected_cols = corrected_cols;
+CZL_info.dp_rows = dp_rows;
+CZL_info.dp_cols = dp_cols;
+CZL_info.dp2_rows = dp2_rows;
+CZL_info.dp2_cols = dp2_cols;
 
-if settings.exploration
-    % Save the (1) z-line clusters (2) cluster trackers (3) distances in microns
-    % (4) distances in microns (5) number removed 
-    save(fullfile(save_path, strcat(output_filename,'_zlines.mat')), ...
-        'zline_clusters', 'cluster_tracker','distances_no_nan', ...
-        'distances_um', 'rmCount');
-else 
-    %Save a struct
-    CZL_struct = struct(); 
-    CZL_struct.zline_clusters = zline_clusters; 
-    CZL_struct.cluster_tracker = cluster_tracker; 
-    CZL_struct.distances_no_nan = distances_no_nan; 
-    CZL_struct.distances_um = distances_um; 
-    CZL_struct.rmCount = rmCount; 
-
-    %Append summary file with OOP 
-    save(fullfile(im_struct.save_path, strcat(im_struct.im_name,...
-       '_OrientationAnalysis.mat')), 'CZL_struct', '-append');
-end   
+% Save .mat file if requested
+if saveResults
+    new_filename = appendFilename( save_info.save_path, ...
+        strcat(save_info.save_name,'.mat') ); 
+    
+    save(fullfile(save_info.save_path, new_filename), ...
+        'CZL_info', 'CZL_results');
+end 
 
 end 
