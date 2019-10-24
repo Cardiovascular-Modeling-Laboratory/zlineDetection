ff23620d71bdf71de9c9d4aaac18ac21eee8a8b3
tessamorris <tessam@uci.edu>
2019-10-24 14:34:30 -0700

    Create manual method of removing background Post analysis summary method

---
+++ functions/CSmanualBackgroundrmv.m
@@ -0,0 +1,51 @@
+% This function will be used to eliminate portions of the background that
+% should be eliminated from analysis 
+
+%% Select coverslip summary file
+% Select the coverslip summary file
+[ CS_name, CS_path,~ ] = load_files( {'*CS_Summary*.mat'}, ...
+    'Select CS summary file for coverslip you would like to manually review...', pwd);
+
+% Load the current coverslip
+CS_data = load(fullfile(CS_path{1},CS_name{1,1})); 
+
+% Get z-line image names 
+zline_images = CS_data.CS_results.zline_images; 
+
+%% Select orientation analysis files 
+n = length(zline_images); 
+FOV_names = cell(n,1); 
+FOV_paths = cell(n,1); 
+% Select the orientation analysis files 
+for k = 1:n
+    clc; 
+    [p,f,e] = fileparts(fullfile(CS_path{1},zline_images{n})); 
+    likelypath = fullfile(p,f); 
+    [ temp_name, temp_path,~ ] = load_files( {'*OrientationAnalysis*.mat'}, ...
+        'Select the orientation analysis file...', likelypath);
+    FOV_names{k,1} = temp_name{1}; 
+    FOV_paths{k,1} = temp_path{1}; 
+end
+
+%% Curate FOVs 
+% For each FOV display the image and ask if the user would like to
+% eliminate part of the background 
+
+% Select the orientation analysis files 
+for k = 1:n
+    % Load the labeled image
+    currentFOV = load(fullfile(FOV_paths{k,1},FOV_names{k,1}));
+    % Load settings
+    settings = currentFOV.settings; 
+    % Load image struct  
+    im_struct = currentFOV.im_struct; 
+    % If exists, load the CZL struct and oop struct - maybe ???
+    
+    % Label the skeleton 
+    [ labeled_im ] = ...
+        labelSkeleton( mat2gray(im_struct.im), im_struct.skel_final_trimmed ); 
+    imshow(labeled_im); 
+    
+    % Ask the user if they'd like to remove parts of the background  
+    
+end
---
+++ functions/modifyROI.m
@@ -0,0 +1,78 @@
+% select_ROI - This function will be used to selct regions of the image 
+% that should either be included or not inlcuded in analysis 
+% 
+% Arguments:
+%   im          - grayscale image 
+%   binim_skel  - binary skeleton 
+%   include     - boolean statement: If include is true then the mask 
+%                   will only include the selected regions. If include is 
+%                   false, the mask will exclude the selected regions. 
+% 
+% Returns:    
+%   mask        - matrix that is 0 where the user doesn't want to include
+%                   data and 1 where the user des want to include data. 
+% 
+% Tessa Morris 
+% The Edwards Lifesciences Center for Advanced Cardiovascular Technology
+% 2418 Engineering Hall
+% University of California, Irvine
+% Irvine, CA  92697-2700
+
+function [ mask ] = modifyROI( im, binim_skel, include )
+
+%Create a mask of all zeros. Any position that is zero at the end of 
+%the analysis will be removed from analysis 
+if include 
+    mask = zeros(size(binim_skel)); 
+else 
+    mask = ones(size(binim_skel)); 
+end 
+
+index = 0;
+hold on
+while index < 1;
+    
+    if include 
+        disp(['Select ROI to include in further analysis'...
+            '(double-click to close the ROI)']);
+    else
+        disp(['Select ROI to exclude from further analysis'...
+            '(double-click to close the ROI)']);
+    end 
+    
+    %Plot the skeleton on top of the image
+    labeled_im  = labelSkeleton( im, binim_skel ); 
+    
+    %Select ROI and overlay the mask 
+    BW = roipoly(labeled_im);
+
+    if include 
+        %Add the selected ROI to the mask 
+    	mask = mask + BW; 
+        %Set any region in the mask that is greater than one equal to 1
+        mask(mask > 1) = 1; 
+    else 
+        %Reverse the ROI mask 
+        BW2 = ~BW; 
+        %Multiply the reversed ROI mask times the mask to set regions equal
+        %to zero 
+        mask = bsxfun(@times, BW2, mask); 
+        %Multiply the reverse ROI mask times the binary skeleton so that it
+        %will be removed for the next iteration
+        binim_skel = bsxfun(@times, binim_skel, mask);
+    end 
+    
+    %Ask the user if they would like to exclude another ROI.
+    b = input('Select another ROI to include? (yes = 0, no = 1): ');
+    if b == 0
+       disp('Image accepted' )
+    else
+       disp('Select another ROI...')
+       index = 1;
+    end
+    
+end
+
+
+end
+
